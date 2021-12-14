59879c312177222db1b226f94fe3ecb9b49c9442
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-13 19:45:18 -0500

    Definiendo clase AR

--- protected/controllers/HolaController.php
+++ protected/controllers/HolaController.php
@@ -3,7 +3,7 @@
 class HolaController extends Controller {
 
     public function actionIndex() {
-        $model=CActiveRecord::model("Users")->findAll();
+        $model=Users::model()->findAll();
         $twitter="@ejemplo.com";
          $this->render("index", array("model"=> $model, "twitter"=> $twitter));
     }
--- protected/models/Users.php
+++ protected/models/Users.php
@@ -1,6 +1,11 @@
 <?php
 
 class Users extends CActiveRecord {
+
+    public static function model($model=__CLASS__) {
+        return parent::model($model);
+    }
+
     public function tableName() {
         return "tbl_user";
     }
